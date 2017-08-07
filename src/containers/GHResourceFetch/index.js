834ace1b5e25e9750bd6cca70716b017d9731c0e
Patrick Bassut <patrickbassut@hotmail.com>
2017-08-07 17:30:44 -0300

    a warning

--- src/containers/GHResourceFetch/index.js
+++ src/containers/GHResourceFetch/index.js
@@ -5,7 +5,7 @@ import { withHandlers, branch, lifecycle, withState, defaultProps, compose } fro
 import { componentDidMount } from 'composables';
 
 export default methodCall => compose(
-  defaultProps({ start: true, token: '51c4dbdc6296622ecc80fa6d11e4383b097b8853' }),
+  defaultProps({ start: true, token: '51c4dbdc6296622ecc80fa6d11e4383b097b8853' }), // No, this token is not valid. That would be foolish
   withState('data', 'setData', []),
   withHandlers({
     request: ({ setData, token, ...props }) => () => {
